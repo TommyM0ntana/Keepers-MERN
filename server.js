d0e073f37ef3292f871c004d950c5fd0b1d8b865
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-04-04 18:19:18 +0200

    add routes

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("Hello world"));
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to the Keeper Manager API" })
+);
+
+//Define the routes for USERS
+
+app.use("/api/users/", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port${PORT}`));
